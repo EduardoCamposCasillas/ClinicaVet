b15f21c5106b6cedd32ecc7957333e4416fe7ec8
MonkiG <raan.heam@gmail.com>
2024-03-20 09:58:35 -0600

    .

--- src/helpers/supabase.js
+++ src/helpers/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import { config } from "./config.js"
  
 export const supabase = createClient(
-    config.REACT_APP_SUPABASE_URL, 
-    config.REACT_APP_SUPABASE_ANON_KEY
+    config.SUPABASE_URL, 
+    config.SUPABASE_ANON_KEY
 );
